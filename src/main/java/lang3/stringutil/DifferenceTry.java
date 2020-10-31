25df9ecd77edd43a52b8b83bb19477a4f196f160
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-10-31 18:40:31 +0900

    difference, unwrap test

--- src/main/java/lang3/stringutil/DifferenceTry.java
+++ src/main/java/lang3/stringutil/DifferenceTry.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class DifferenceTry {
     public static void main(String[] args) {
         System.out.println(StringUtils.difference("abc", "abcde"));  // de
+        System.out.println(StringUtils.difference("abcde", "abc"));  // ""
         System.out.println(StringUtils.difference("abd", "abcde"));  // cde
         System.out.println(StringUtils.difference("cabc", "abcde")); // abcde
         System.out.println(StringUtils.difference("abcd", "abed"));  // ed
--- src/main/java/lang3/stringutil/unwrapTry.java
+++ src/main/java/lang3/stringutil/unwrapTry.java
@@ -6,5 +6,8 @@ public class unwrapTry {
     public static void main(String[] args) {
         System.out.println(StringUtils.unwrap("AABabcBAA", 'A')); // ABabcBA
         System.out.println(StringUtils.unwrap("ABCA", 'A')); // BC
+
+        // System.out.println(StringUtils.unwrap("ababa", "aba")); // 修正中っぽい
+        System.out.println(StringUtils.unwrap("aaa", 'a')); // a
     }
 }
